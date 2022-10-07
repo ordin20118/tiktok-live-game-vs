33c06a192f80b9161143b217aad5b46c36e58e3a
ordin20118 <75382892+ordin20118@users.noreply.github.com>
2022-10-07 09:54:12 +0900

    first commit

---
+++ src/websocket_client/client.py
@@ -0,0 +1,38 @@
+import _thread
+import time
+import datetime
+import asyncio 
+import websockets
+
+def on_message(ws, message):
+    print(message)
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws, close_status_code, close_msg):
+    print("### closed ###")
+
+def on_open(ws):
+    print("Opened connection")
+
+
+
+async def get_connect():
+    async with websockets.connect("ws://localhost:30001") as websocket:
+        # for i in range(1,100,1):
+        #     await websocket.send("Hi server. I'm client" );
+        #     data_rcv = await websocket.recv(); 
+        #     print("data received from server : " + data_rcv); 
+        await websocket.send("Hi server. I'm client" );
+        
+        while True:
+            now = datetime.datetime.now()
+            msg_rcv = await websocket.recv(); 
+            print('[%s] %s' % (now, msg_rcv)) 
+        
+        # print("data received from server : " + data_rcv); 
+
+# connect to server 
+asyncio.get_event_loop().run_until_complete(get_connect());
+#asyncio.get_event_loop().run_forever(get_connect());
